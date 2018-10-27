9e9e8cd87551216738b551ffdf6909d069ba300a
April Copes <antdevelopment1@gmail.com>
2018-10-26 21:56:55 -0400

    Covers topics on Modular Design Patterns to Prepare Type Writer App

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,22 @@
-var hello = alert('hello');
+// =======================
+// Modular Design Patterns
+// =======================
+
+
+// Creates an IIFE
+var myModule = (function() {
+    // Private variables and functions
+    var x = 1;
+
+
+    // Public variables and functions
+    // Returns an object with a key called f1 and its value is a function that returns x
+    return {
+        f1: function () {
+            return x;   
+        }
+    }
+})();
+
+// Calls myModule and invokes the key's function
+console.log(myModule.f1());
