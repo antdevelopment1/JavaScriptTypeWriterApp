6b2ed1b3657f723d45322ca8283a56aa05430046
April Copes <antdevelopment1@gmail.com>
2018-10-26 23:45:43 -0400

    Creates an object that stores function calls for  indicators

--- scripts/dataModule.js
+++ scripts/dataModule.js
@@ -8,3 +8,21 @@ var appData = {
     results: {wpm: 0, wpmChange: 0, cpm: 0, cpmChange: 0, accuracy: 0, accuracyChange: 0}
 };
 console.log(appData);
+
+// ================
+// Public Variables
+// ================
+var dataModule = (function() {
+    // Indicators 
+    return {
+        setTestTime: function(x){}, // Sets the toal test time to x
+        initializeTimeLeft: function() {}, // Initialize time left to the total test time
+        startTest: function() {}, // Starts the test
+        endTest: function() {}, // Ends the test
+        reduceTime: function() {}, // Reduces the time by 1 second
+        timeLeft: function() {}, // Check is the test has already ended
+        testEnded: function() {}, // Check is the test has already ended
+        testStarted: function() {} // Checks if the test has started
+    }
+
+}());
