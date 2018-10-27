f7ae7117055824b4d1159445d768df2e6ab28362
April Copes <antdevelopment1@gmail.com>
2018-10-27 13:03:55 -0400

    Adds words object appData object

--- scripts/dataModule.js
+++ scripts/dataModule.js
@@ -5,7 +5,8 @@
 // Indicators / Test Control / Test Tesults
 var appData = {
     indicators: {testStarted: false, testEnded: false, totalTestTime: 0, timeLeft: 0},
-    results: {wpm: 0, wpmChange: 0, cpm: 0, cpmChange: 0, accuracy: 0, accuracyChange: 0}
+    results: {wpm: 0, wpmChange: 0, cpm: 0, cpmChange: 0, accuracy: 0, accuracyChange: 0},
+    words: {currentWordIndex: 0},
 };
 console.log(appData);
 
