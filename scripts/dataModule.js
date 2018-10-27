086b55c894c41c65cf9b28b3cce01e9dcf1bf990
April Copes <antdevelopment1@gmail.com>
2018-10-26 23:26:22 -0400

    Creates appData object

---
+++ scripts/dataModule.js
@@ -0,0 +1,10 @@
+// =================
+// Private Variables
+// =================
+
+// Indicators / Test Control / Test Tesults
+var appData = {
+    indicators: {testStarted: false, testEnded: false, totalTestTime: 0, timeLeft: 0},
+    results: {wpm: 0, wpmChange: 0, cpm: 0, cpmChange: 0, accuracy: 0, accuracyChange: 0}
+};
+console.log(appData);
