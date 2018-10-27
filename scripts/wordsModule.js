4d8a68010a03a292a73a6aa649b6a19d72aad3b5
April Copes <antdevelopment1@gmail.com>
2018-10-27 15:03:43 -0400

    Initial set up for words Module

---
+++ scripts/wordsModule.js
@@ -0,0 +1,27 @@
+// =============
+// Words Module
+// ==============
+
+var wordsModule = (function() {
+
+    // =================
+    // Private Variables
+    // =================
+
+    var words = [
+        'word1 word2 word2 etc',
+        'word1 word2 word2 etc',
+        'word1 word2 word2 etc'
+    ];
+
+    // ================
+    // Public Variables
+    // ================
+    return {
+
+        getWords(textNumber) {
+            return words[textNumber];
+        }
+
+    }
+}());
