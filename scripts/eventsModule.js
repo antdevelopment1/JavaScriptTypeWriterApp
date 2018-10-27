d6adef70481a7a077286aef2e961dfd307b5cda9
April Copes <antdevelopment1@gmail.com>
2018-10-27 14:56:26 -0400

    Initial structure for eventsModule

---
+++ scripts/eventsModule.js
@@ -0,0 +1,32 @@
+// =============
+// Events Module
+// ==============
+
+var eventsModule = (function() {
+
+    // =================
+    // Private Variables
+    // =================
+    var addEventListeners = function() {
+
+        // Character typing event listener
+
+        // Click on download button event listener
+
+        // Click on restart button event listener
+
+    }
+ 
+    // ================
+    // Public Variables
+    // ================
+    return {
+
+        // Init function intializes the test before start
+        init: function(duration, testNumber) {
+
+            addEventListeners();
+            
+        }
+    };  
+}());
