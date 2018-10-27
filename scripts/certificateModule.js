1e27880e63634645ceea57b97d7a34c9fac1833f
April Copes <antdevelopment1@gmail.com>
2018-10-27 14:49:08 -0400

    Initial structure for certificateModule

---
+++ scripts/certificateModule.js
@@ -0,0 +1,21 @@
+// ==================
+// Certificate Module
+// ==================
+
+var certificateModule = (function() {
+
+// =================
+// Private Variables
+// =================
+
+// No Private Variables At This Time
+
+
+// ================
+// Public Variables
+// ================
+    return {
+        generateCertificate: function() {},
+    };
+
+}());
